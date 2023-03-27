fe42767089001aebdd8e7633ebce159e6120e944
Tamir Rojas <tyr.rojas@gmail.com>
2023-03-27 17:08:35 +0200

    my progress so far, schemas are being created at this time after documenting myself about models and stuff

--- database/migrations/2023_03_27_122130_add_info_to_users.php
+++ database/migrations/2023_03_27_122130_add_info_to_users.php
@@ -12,13 +12,17 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
+            $table->bigIncrements('user_id')->unique();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+            $table->string('last_name');
+            $table->string();
+            //
+
+        });
+        Schema::create('podcasts', function(Blueprint $table){
+            $table->foreignId('user_id')->constrained();
+            $table->
+
         });
     }
 
@@ -27,6 +31,8 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
     }
 };
